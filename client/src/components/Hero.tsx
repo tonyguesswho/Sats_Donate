6d0b701a54992e769ea5ed5dc61322e114853879
Anthony <anthonyu234@gmail.com>
2023-02-22 14:01:26 +0100

    Genrate qrcode and donate

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -4,13 +4,49 @@ import {
   Container,
   Text,
   Button,
-  Stack
+  Stack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea
 } from '@chakra-ui/react';
 
 import { Link } from 'react-router-dom';
+import {useRef, useState} from 'react';
+import axios from 'axios'
 
 
 export default function Landing() {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  let [projectDescription, setProjectDescription] = useState('')
+  let [projectname, setProjectname] = useState('')
+
+  const initialRef = useRef(null)
+  const finalRef = useRef(null)
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    axios.post(`${process.env.REACT_APP_API_URL}/project`, {name:projectname, description:projectDescription})
+      .then(response => {
+        setProjectDescription("")
+        setProjectname("")
+        onClose()
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+
   return (
     <>
       <Container maxW={'3xl'}>
@@ -31,13 +67,14 @@ export default function Landing() {
           <Text color={'gray.500'}>
             Donate to your favaorite projects and organisations privately using Bitcoin and the ligthning Network
           </Text>
+
           <Stack
-            direction={'column'}
-            spacing={3}
+            spacing={6}
+            direction={'row'}
             align={'center'}
             alignSelf={'center'}
             position={'relative'}>
-            <Link to="/projects"><Button
+          <Link to="/projects"><Button
               colorScheme={'green'}
               bg={'green.400'}
               rounded={'full'}
@@ -47,9 +84,48 @@ export default function Landing() {
               }}>
              View Projects
             </Button></Link>
-          </Stack>
+          <Button onClick={onOpen} rounded={'full'} px={6}>
+            Create Project
+          </Button>
+        </Stack>
         </Stack>
       </Container>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your project</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl isRequired>
+              <FormLabel>Project name</FormLabel>
+              <Input ref={initialRef} placeholder='Name' onChange={e => setProjectname(e.target.value)} value={projectname} />
+            </FormControl>
+
+            <FormControl mt={4} isRequired>
+              <FormLabel>Project description</FormLabel>
+              <Textarea
+                value={projectDescription}
+                onChange={e => setProjectDescription(e.target.value)}
+                placeholder='Here is a sample placeholder'
+                size='sm'
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='green' mr={3} onClick={handleSubmit}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -7,124 +7,166 @@ import {
   SimpleGrid,
   Flex,
   useColorModeValue,
-  Button
+  Button,
+  FormControl,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  useClipboard
 } from '@chakra-ui/react';
+// import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import QRCode from 'react-qr-code'
 
-const projects = [
-  {
-    heading: 'Cancer Care',
-    content: 'Help support therapy for patients undergoing chemo',
-    icon: (
-      <svg
+function ProjectItem({ name, description, index, id }:any) {
+  const [amount, setAmount] = useState(1000);
+  const [invoice, setInvoice] = useState(null)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { onCopy, value, setValue, hasCopied } = useClipboard("");
+
+  const handleDonate = (id:number) => {
+    console.log(id, "pppp", amount)
+    axios.post(`${process.env.REACT_APP_API_URL}/donation`, {amount, projectId:id})
+      .then(async(response )=> {
+        if(response.statusText === "OK"){
+          console.log(response, "gggg")
+          setInvoice(response.data.data.paymentRequest)
+          setValue(response.data.data.paymentRequest)
+          const dataInvoiceStream = await (await fetch(`${process.env.REACT_APP_API_URL}/node/check`)).json()
+          onOpen()
+        }
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+
+  const handleAmountChange = (value:any) => setAmount(value)
+
+  return (
+    <>
+    <Box
+    key={index}
+    bg={useColorModeValue('gray.100', 'gray.700')}
+    p={6}
+    rounded="lg"
+    textAlign="center"
+    pos="relative"
+  >
+    <Flex
+      p={2}
+        w="max-content"
+        color="white"
+        bgGradient="linear(to-br, #228be6, #15aabf)"
+        rounded="md"
+        marginInline="auto"
+        pos="absolute"
+        left={0}
+        right={0}
+        top="-1.5rem"
+        boxShadow="lg"
+      >
+        {<svg
         width={36}
         height={36}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-        ></path>
-      </svg>
-    )
-  },
-  {
-    heading: 'Invoicing',
-    content: 'Webhooks are wired up to automatically email customers PDF receipts and invoices.',
-    icon: (
-        <svg
-          width={36}
-          height={36}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-          ></path>
-        </svg>
-      )
-  },
-  {
-    heading: 'API Included',
-    content: 'Roll your own API to easily connect with other apps or services. Pull in updates.',
-    icon: (
-        <svg
-          width={36}
-          height={36}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-          ></path>
-        </svg>
-      )
-  }
-];
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+      ></path>
+      </svg>}
+    </Flex>
+
+    <chakra.h3 fontWeight="semibold" fontSize="2xl" mt={6}>
+      {name}
+    </chakra.h3>
+    <Text fontSize="md" mt={4} w={300}>
+      {description}
+    </Text>
+    <FormControl isRequired>
+      <NumberInput defaultValue={2000} min={10} onChange={handleAmountChange} value={amount}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+    </FormControl>
+    <Button
+    onClick={() => handleDonate(id)}
+      colorScheme={'green'}
+      bg={'green.400'}
+      mt={6}
+      rounded={'full'}
+      px={6}
+      _hover={{
+        bg: 'green.300',
+      }}>
+     Donate
+    </Button>
+  </Box>
+  <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Ligthning Payment Invoice</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          <Box  textAlign="center">
+          <QRCode value={invoice || ""}/>
+          <Text mt={4}>{invoice}</Text>
+          <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
+          </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+  </>
+  );
+}
 
 const Features = () => {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/project`)
+    .then(response => {
+      const {data} = response
+      setProjects(data.data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+
   return (
     <Container maxW="6xl" p={{ base: 5, md: 10 }}>
       <chakra.h3 fontSize="4xl" fontWeight="bold" mb={20} textAlign="center">
         Projects
       </chakra.h3>
       <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} placeItems="center" spacing={10} mb={4}>
-        {projects.map((project, index) => (
-          <Box
-            key={index}
-            bg={useColorModeValue('gray.100', 'gray.700')}
-            p={6}
-            rounded="lg"
-            textAlign="center"
-            pos="relative"
-          >
-            <Flex
-              p={2}
-              w="max-content"
-              color="white"
-              bgGradient="linear(to-br, #228be6, #15aabf)"
-              rounded="md"
-              marginInline="auto"
-              pos="absolute"
-              left={0}
-              right={0}
-              top="-1.5rem"
-              boxShadow="lg"
-            >
-              {project.icon}
-            </Flex>
-            <chakra.h3 fontWeight="semibold" fontSize="2xl" mt={6}>
-              {project.heading}
-            </chakra.h3>
-            <Text fontSize="md" mt={4}>
-              {project.content}
-            </Text>
-            <Button
-              colorScheme={'green'}
-              bg={'green.400'}
-              mt={6}
-              rounded={'full'}
-              px={6}
-              _hover={{
-                bg: 'green.300',
-              }}>
-             Donate
-            </Button>
-          </Box>
+        {projects.map((project:any, index) => (
+          <ProjectItem name={project.name} description={project.description} index={index} id={project.id} />
+
         ))}
       </SimpleGrid>
     </Container>
